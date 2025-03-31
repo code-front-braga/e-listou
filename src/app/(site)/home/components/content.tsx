b9a7c0417b4c3292e77fb449e525071e0d4b7e48
Leonardo Braga <programer_frontbraga@outlook.com>
2025-03-30 22:34:49 -0300

    feat: finish site home page styles

--- src/app/(site)/home/components/content.tsx
+++ src/app/(site)/home/components/content.tsx
@@ -1,8 +1,68 @@
-export default function Content() {
-  return (
-    <div>
-      <h1>Content Page</h1>
-      <p>This is the content page.</p>
-    </div>
-  )
+'use client';
+
+import { motion } from 'motion/react';
+import Link from 'next/link';
+import { FaOpencart } from 'react-icons/fa';
+
+const opacityVariants = {
+	visible: { opacity: 1 },
+	hidden: { opacity: 0 },
+};
+
+export function Content() {
+	return (
+		<div className="relative z-10 m-auto flex flex-col items-center gap-1">
+			<div className="relative flex items-center justify-center gap-2 overflow-hidden">
+				<motion.h1
+					initial={{ opacity: 0, x: 50 }}
+					animate={{ opacity: 1, x: 0 }}
+					transition={{ duration: 1.4, ease: 'easeOut', delay: 1.2 }}
+					className="font-poiretOne text-3xl font-semibold text-white sm:text-4xl"
+				>
+					<span className="text-cabaret">e-</span>
+					<span className="text-cabaret">L</span>istou
+					<motion.span
+						variants={opacityVariants}
+						initial="hidden"
+						whileInView="visible"
+						transition={{ duration: 1.4, ease: 'linear', delay: 2.6 }}
+						className="text-cabaret font-lato"
+					>
+						!
+					</motion.span>
+				</motion.h1>
+				<motion.div
+					initial={{ x: -50 }}
+					animate={{ x: 0 }}
+					transition={{ duration: 2.2, ease: 'anticipate', delay: 0.6 }}
+					className="z-0 flex items-center justify-center rounded-full bg-white p-1.5"
+				>
+					<FaOpencart size={24} className="text-christalle" />
+				</motion.div>
+			</div>
+			<motion.p
+				variants={opacityVariants}
+				initial="hidden"
+				whileInView="visible"
+				transition={{ duration: 1, ease: 'linear', delay: 1.6 }}
+				className="text-lg text-white sm:text-xl"
+			>
+				Seu supermercado, do seu jeito.
+			</motion.p>
+			<motion.button
+				variants={opacityVariants}
+				initial="hidden"
+				whileInView="visible"
+				transition={{ duration: 1, ease: 'anticipate', delay: 1.8 }}
+				className="mt-1.5 rounded-full bg-white p-1.5 px-3 shadow-2xl sm:w-full"
+			>
+				<Link
+					href="/auth/register"
+					className="text-christalle font-semibold sm:text-xl"
+				>
+					Crie sua conta
+				</Link>
+			</motion.button>
+		</div>
+	);
 }
--- src/app/(site)/home/page.tsx
+++ src/app/(site)/home/page.tsx
@@ -1,10 +1,17 @@
 import { SiteBackground } from '../components/site-background';
 import { SiteSection } from '../components/site-section';
+import { BackgroundEffect } from './components/background-effect';
+import { Content } from './components/content';
 
 export default function SiteHomePage() {
 	return (
-		<SiteSection id="site-home" className="bg-christalle min-h-svh w-full relative z-10">
+		<SiteSection
+			id="site-home"
+			className="bg-christalle relative z-10 flex min-h-svh w-full"
+		>
 			<SiteBackground />
+			<BackgroundEffect />
+			<Content />
 		</SiteSection>
 	);
 }
